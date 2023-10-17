876e1aeda700deb7096ddd344a882a435656bd14
abinayagovintharaj <148181790+abinayagovintharaj@users.noreply.github.com>
2023-10-17 11:16:22 +0530

    Add files via upload

---
+++ challenge2.1.py
@@ -0,0 +1,21 @@
+class BankAccount:
+  def __init__(self, account_number, account_number account_holder_nane,initial_balance=0.0):
+    self.__account_number=account_number
+ self.__account_holders_name=account_holder_name
+self.__account_balance=initial_balance
+   def deposit(self, amount):
+     if amount > 0:
+       self __account_balance+=amount
+print("Deposite
+  ₹
+{}.new balance:{}".format(amount,self.___account_balance))
+else:
+print("Invalid deposit amount.pleasw deposit a positive amount.")
+def withdraw(self,amount):
+  if amount> 0 and amount.")
+           
+
+ 
+       
+  
+  
